a397bcce779a73f26566b009061cee4a51b2ae5f
Ubuntu <ubuntu@ip-172-31-71-110.ec2.internal>
2026-08-28 10:10:26 +0000

    Fix frontend API URLs

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,10 +1,10 @@
 const config = {
   urls: {
     auth: {
-      login: process.env.LOGIN_URL || 'http://0.0.0.0:8081/auth/login',
-      logout: process.env.LOGOUT_URL || 'http://0.0.0.0:8081/auth/logout',
-      validate: process.env.VALIDATE_URL || 'http://0.0.0.0:8081/auth/validate',
-      register: process.env.VALIDATE_URL || 'http://0.0.0.0:8081/auth/register'
+      login: '/api/auth/login',
+      logout: '/api/auth/logout',
+      validate: '/api/auth/validate',
+      register: '/api/auth/register'
     }
   }
 }
